f5192f18ca303ab169dfdbc22d86a974df0e5b19
Duc238 <92005265+Duc238@users.noreply.github.com>
2024-03-15 14:15:05 +0700

    Add files via upload

---
+++ Test/Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding=Encoding.UTF8;
+            int NumberTotal, NumberStart=0, NumberSum=0;
+            Console.WriteLine("Nhập số các số muốn tính trung bình: ");
+            NumberTotal=Convert.ToInt32(Console.ReadLine());
+            while(NumberStart<=NumberTotal) 
+            {
+                Console.WriteLine("Nhập vào số thứ {0}: ",NumberStart);
+                NumberSum += Convert.ToInt32(Console.ReadLine());
+                NumberStart++;
+            }
+            Console.WriteLine("Giá trị trung bình= {0}", (double)NumberSum/NumberTotal);
+        }
+    }
+}
